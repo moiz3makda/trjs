238b3ea757bb6bdb350f56bf8918a4cee4f1ea6e
moiz3makda <96173488+moiz3makda@users.noreply.github.com>
2022-05-12 16:50:54 +0530

    data types
    learned primitive and reference data types

---
+++ 2nd.js
@@ -0,0 +1,8 @@
+let num1=6;
+let num2=num1;
+console.log("value is numl is", num1);
+console.log("value is num2 is", num2);
+num1++;
+console.log("after incrementing num1")
+console.log("value is numl is", num1);
+console.log("value is num2 is", num2);
---
+++ 3rd.js
@@ -0,0 +1,4 @@
+const fruits=["apple", "mango"];
+fruits.push("banana");
+fruits[1]="cherry";
+console.log(fruits);
